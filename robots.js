6e0072e16a8af678a5210607a2768a66d8aa5374
ndeberuti <ndeberuti@gmail.com>
2020-11-05 09:29:02 -0300

    Add goal oriented robot with pathfinding and find route function

--- robots.js
+++ robots.js
@@ -13,6 +13,31 @@ const { roadGraph, mailRoute } = require("./roads");
       console.log(`Moved to ${action.direction}`);
     }
   }
+
+  function findRoute(graph, from, to) {
+    let work = [{at: from, route: []}];
+    for (let i = 0; i< work.length; i++) {
+      let {at, route} = work[i];
+      for (let place of graph[at]) {
+        if (place == to) return route.concat(place);
+        if (!work.some(w => w.at == place)) {
+          work.push({at: place, route: route.concat(place)});
+        }
+      }
+    }
+  }
+
+  function goalOrientedRobot({place, parcels}, route) {
+    if (route.length == 0) {
+    let parcel = parcels[0];
+    if (parcel.place != place) {
+    route = findRoute(roadGraph, place, parcel.place);
+    } else {
+    route = findRoute(roadGraph, place, parcel.address);
+    }
+    }
+    return {direction: route[0], memory: route.slice(1)};
+    }
   
   function randomRobot(state) {
     return { direction: randomPick(roadGraph[state.place]) };
@@ -28,5 +53,6 @@ const { roadGraph, mailRoute } = require("./roads");
 module.exports = {
     routeRobot,
     runRobot,
-    randomRobot
+    randomRobot,
+    goalOrientedRobot
 }
