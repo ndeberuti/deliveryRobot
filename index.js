3f10f9cbfbbcf6c7fcafdf69cc53fe8543aaf4c8
ndeberuti <ndeberuti@gmail.com>
2020-11-05 09:29:20 -0300

    Add call to run goal oriented robot

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const VillageState  = require("./village");
-const { randomRobot, routeRobot, runRobot } = require("./robots");
+const { randomRobot, routeRobot, runRobot, goalOrientedRobot } = require("./robots");
 const { mailRoute } = require("./roads");
   
 
 runRobot(VillageState.random(), randomRobot);
 
 runRobot(VillageState.random(), routeRobot, mailRoute);
+
+runRobot(VillageState.random(), goalOrientedRobot, []);
